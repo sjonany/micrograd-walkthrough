ae593f6864a1fe51d3ac078eab6f97e746fdac57
Stephen <sjonany@gmail.com>
2023-12-16 11:43:14 -0800

    Added value class and demo

---
+++ micrograd/engine.py
@@ -0,0 +1,27 @@
+class Value:
+    """An expression DAG node that stores fwd pass value and its gradient"""
+
+    def __init__(self, data, label, children=(), op=""):
+        self.data = data
+        self.label = label
+        self.children = children
+        self.op = op
+
+    def __mul__(self, other):
+        return Value(
+            self.data * other.data,
+            f"({self.label} * {other.label})",
+            children=(self, other),
+            op="*",
+        )
+
+    def __add__(self, other):
+        return Value(
+            self.data + other.data,
+            f"({self.label} + {other.label})",
+            children=(self, other),
+            op="+",
+        )
+
+    def __repr__(self):
+        return f"{self.label} : {self.data}"
